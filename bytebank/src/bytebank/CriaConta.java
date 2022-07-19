b0f1cd496014d10cec13bc80c9050d5019b5a610
Gabriel Peres <gabriel.2000mourad@gmail.com>
2022-07-18 22:07:45 -0300

    segunda instancia de uma conta

--- bytebank/src/bytebank/CriaConta.java
+++ bytebank/src/bytebank/CriaConta.java
@@ -4,11 +4,18 @@ public class CriaConta {
 
 	public static void main(String[] args) {
 		
-		Conta conta = new Conta();
-		conta.saldo = 200;
-		conta.titular = "Gabriel";
-		System.out.println(conta.saldo);
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 200;
+		primeiraConta.titular = "Gabriel";
+		System.out.println("Primeira Conta:" + "\nTitular: " + primeiraConta.titular + "\nSaldo: " + primeiraConta.saldo);
 		
+		primeiraConta.saldo+=100;
+		//System.out.println(primeiraConta.saldo);
+		
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 500;
+		segundaConta.titular = "Carlos";
+		System.out.println("Segunda Conta:" + "\nTitular: " + segundaConta.titular + "\nSaldo: " + segundaConta.saldo);
 	}
 
 }
