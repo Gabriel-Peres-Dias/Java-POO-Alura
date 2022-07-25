ddf491fa9a10e3965f2579f7e7a9f5837f6422f8
Gabriel Peres Dias <gabriel.suporte123@gmail.com>
2022-07-25 18:38:59 -0300

    reajustando a pasta

---
+++ bytebank-herdado-conta/src/Conta.java
@@ -0,0 +1,81 @@
+
+
+public class Conta {
+		
+		private double saldo;
+		private int agencia;
+		private int numero;
+		private Cliente titular;
+		private static int total = 0;
+		
+		
+		
+	
+	public Conta (int agencia, int numero) {
+		Conta.total++;
+		this.agencia = agencia;
+		this.numero = numero;
+		System.out.println("Criando uma conta " + this.agencia);
+	}
+		
+		public double getSaldo() {
+			return saldo;
+		}
+
+		public void setSaldo(double saldo) {
+			this.saldo = saldo;
+		}
+
+		public int getAgencia() {
+			return agencia;
+		}
+
+		public void setAgencia(int agencia) {
+			this.agencia = agencia;
+		}
+
+		public int getNumero() {
+			return numero;
+		}
+
+		public void setNumero(int numero) {
+			this.numero = numero;
+		}
+
+		public Cliente getTitular() {
+			return titular;
+		}
+
+		public void setTitular(Cliente titular) {
+			this.titular = titular;
+		}
+
+		public void deposita(double valor){
+		this.saldo += valor;
+		System.out.println(this.saldo);
+	}
+	
+		public boolean saca(double valor){
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} 
+			return false;
+	}
+	
+		public boolean transfere(double valor, Conta destino) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		} 
+			return false;	
+		}
+	
+		public static int getTotal() {
+		return total;
+	}
+	
+	
+
+}
