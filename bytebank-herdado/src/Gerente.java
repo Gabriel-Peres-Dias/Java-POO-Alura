a9b3961f1082a0ed7ff517a04cd84a0b0a4c0ee5
Gabriel Peres Dias <gabriel.suporte123@gmail.com>
2022-07-24 21:39:34 -0300

    Mais uma classe de teste do polimorfismo

--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -24,7 +24,7 @@ public class Gerente extends Funcionario{
 	
 	  public double getBonificacao() { 
 		  return super.getBonificacao() + super.getSalario(); 
-		  }
+	  }
 	 
 	
 	
--- bytebank-herdado/src/TesteReferencias.java
+++ bytebank-herdado/src/TesteReferencias.java
@@ -12,10 +12,14 @@ public class TesteReferencias {
 		EditorVideo e1 = new EditorVideo();
 		e1.setSalario(2500);
 		
+		Designer d1 = new Designer();
+		g1.setSalario(2000);
+		
 		ControleBonificacao controle = new ControleBonificacao();
 		controle.registra(g1);
 		controle.registra(f1);
 		controle.registra(e1);
+		controle.registra(d1);
 		
 		System.out.println(controle.getSoma());
 		
